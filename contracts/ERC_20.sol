246cba17210a3abc480828278ac6707623158bd8
Ankit Kumar <animusk7765@gmail.com>
2022-12-15 19:45:21 +0530

    adding mapping for depositor enquires

--- contracts/ERC_20.sol
+++ contracts/ERC_20.sol
@@ -3,34 +3,44 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
-contract MyToken  {
-    uint[5] public  supplies;
-    uint public count=supplies.length ;
+contract MyToken {
+    uint256[5] public supplies;
+    uint256 public count = supplies.length;
+    mapping(address => uint256) public depositor_slab;
     string public _name;
     string public symbol;
+
     constructor() {
         symbol = "MTK";
         _name = "MyToken ";
     }
 
-    mapping (address => uint) public  depositor_slab;
+    function name() external view returns (string memory) {
+        return _name;
+    }
 
-   function deposit(uint _amount)  external {
-        if(supplies[count-1]+_amount  < count * 100 ){
-            supplies[count-1]+=_amount;
-            depositor_slab[msg.sender]=count;
-        }
-        else {
-           uint rem = (count * 100)-supplies[count-1];
-           
-           supplies[count -1]+= rem;
-           if(depositor_slab[msg.sender]==0){
-               depositor_slab[msg.sender]=count;
-           }
-           count--;
-          
-        }
+    function deposit(uint256 _amount) external {
+        if (supplies[count - 1] + _amount < count * 100) {
+            supplies[count - 1] += _amount;
+            depositor_slab[msg.sender] = count;
+        } else {
+            uint256 rem = (count * 100) - supplies[count - 1];
 
-   
+            supplies[count - 1] += rem;
+            if (depositor_slab[msg.sender] == 0) {
+                depositor_slab[msg.sender] = count;
+            }
+            count--;
+        }
+    }
+    function tranfer(address _to, uint256 _amount) external returns (bool) {
+        require(
+            balances[msg.sender] >= _amount,
+            "not enough balance to transfer to address"
+        );
+        balances[msg.sender] -= _amount;
+        balances[_to] += _amount;
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
 }
